37f3846be13cc9e14f85924946aa4539af9561b4
Janani <jananise@gmail.com>
2017-07-25 10:16:43 +0530

    remove duplicates

---
+++ src/com/janani/question4/Main.java
@@ -0,0 +1,28 @@
+package com.janani.question4;
+
+import java.util.LinkedHashSet;
+
+public class Main {
+
+	static Integer[] removeDuplicates(Integer[] input) {
+
+		LinkedHashSet<Integer> set = new LinkedHashSet<>();
+
+		for (Integer a : input) {
+			set.add(a);
+		}
+
+		Integer[] output = set.toArray(new Integer[set.size()]);
+		return output;
+
+	}
+
+	public static void main(String[] args) {
+
+		Integer[] input = { 1, 4, 5, 8, 5, 2 };
+		Integer[] output = removeDuplicates(input);
+		for (Integer a : output) {
+			System.out.println(a);
+		}
+	}
+}
